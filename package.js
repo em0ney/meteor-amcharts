366825130920dda59465653d38f199a18ea13e0e
Elliott Spira <elliottspira@gmail.com>
2015-04-14 09:30:46 +1000

    merging changes from res-io to expose RGBColor outside of file scope to package scope. prepping for publish

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
-  summary: "DEPRACATED: use em0ney:amcharts",
-  name: "mrt:amcharts",
-  version: "3.13.1",
+  summary: "Plugin for easy installation of amcharts free version in meteor projects",
+  name: "em0ney:amcharts",
+  version: "3.13.1.1",
   git: "https://github.com/em0ney/meteor-amcharts.git",
   documentation: "README.md"
 });
